3ec12eaaad1e8b3956b4d08cf41c070fa48d6770
omar5867 <bmamanicc@unsa.edu.pe>
2023-10-21 11:40:17 -0500

    Detección de variables vacias

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,8 +2,22 @@
     <p>
         Esto solo aparece porque la funciòn que tenemos nos dice que aparecera solo si 4 es menor que 5.
     </p>
-@elseif (2<5)
+@elseif (2==5)
     <p>
         Esto solo aparec porque se evito el tener un caso anterior con elif.
     </p>
+@else
+    <p>
+        Ninguna de las condiciones se cumplio
+    </p>
 @endif
+@empty($ubicación)
+    <!-- Solo se ejecutara si detecta que la ubicación es vacia-->
+    <h2>
+        No se definio la variable ubicación
+    </h2>
+@endempty
+
+@isset($record)
+    <!--Solo se ejecutarà si una de nuestras variables esta definida-->
+@endisset
